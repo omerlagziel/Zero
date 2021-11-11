6762081e0c260c850f0a873963055a2ad3512205
L <>
2021-11-11 22:39:24 +0200

    #1 - Modified name everywhere, including file and folder names

--- packages/mobile/android/app/src/main/java/com/zero/MainActivity.java
+++ packages/mobile/android/app/src/main/java/com/zero/MainActivity.java
@@ -1,4 +1,4 @@
-package com.myprojectname;
+package com.zero;
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
@@ -11,7 +11,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "myprojectname";
+    return "zero";
   }
 
   @Override
--- packages/mobile/ios/zeroTests/zeroTests.m
+++ packages/mobile/ios/zeroTests/zeroTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface myprojectnameTests : XCTestCase
+@interface zeroTests : XCTestCase
 
 @end
 
-@implementation myprojectnameTests
+@implementation zeroTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
