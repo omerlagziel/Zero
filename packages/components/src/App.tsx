b9ce0213b8efa92d1c85ff4a66bf8065fdac9010
L <>
2021-11-18 19:38:53 +0200

    #1 - Fix web-nextjs build bug
    More information here https://github.com/brunolemos/react-native-web-monorepo/issues/69

--- packages/components/src/App.tsx
+++ packages/components/src/App.tsx
@@ -1,12 +1,4 @@
 import React from 'react'
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native'
 
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
