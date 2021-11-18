7bd198621e251904554e9e95d827d697fc3c454a
L <>
2021-11-18 18:39:36 +0200

    #1 - Fix NextJS not working

---
+++ packages/components/src/App.web.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native'
+
+export function App() {
+  return <Text>Hello world !</Text>
+}
