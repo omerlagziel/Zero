3671e789a9dfafa04a5e4e3ee676ec32f92be2c1
L <l@ls-macbook-pro.mynet>
2021-11-22 20:47:40 +0200

    #1 - Added buttons

--- packages/components/src/features/review/Review.tsx
+++ packages/components/src/features/review/Review.tsx
@@ -9,7 +9,7 @@ import {
 } from 'react-native'
 import { useSelector, useDispatch } from 'react-redux'
 import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs'
-// import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
 import { RootState } from '../../store'
 import { decrement, increment } from './slice'
@@ -21,51 +21,64 @@ type Props = {
 }
 
 export default ({ navigation }: Props) => {
+  // return (
+  //   <View style={{ flex: 1 }}>
+  //     <View style={{ backgroundColor: 'blue', flex: 1 }}>
+  //       <Text>geg</Text>
+  //     </View>
+  //     <View style={{ backgroundColor: 'red', height: 300 }}></View>
+  //   </View>
+  // )
   return (
-    <View style={styles.container}>
-      <ScrollView style={styles.reviewContainer}>
-        <ReviewCard
-          title="John Doe"
-          subtitle="2 days ago"
-          description="Food was great as always, highly recommend this place! Will definitely come back soon. Only note is to keep the music a little less loud."
-          fullscreen
-        />
-        <ScrollView
-          style={styles.replyOptions}
-          contentContainerStyle={styles.replyOptionsContainer}
-          horizontal
-          showsHorizontalScrollIndicator={false}
-        >
-          <TouchableOpacity style={styles.replyOption}>
-            <Text style={styles.replyOptionText}>Thank you John!</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.replyOption}>
-            <Text style={styles.replyOptionText}>Thanks John!</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.replyOption}>
-            <Text style={styles.replyOptionText}>Many thanks John!</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.replyOption}>
-            <Text style={styles.replyOptionText}>Appreciate it John!</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.replyOption}>
-            <Text style={styles.replyOptionText}>Customize..</Text>
-          </TouchableOpacity>
+    <KeyboardAwareScrollView contentContainerStyle={styles.container}>
+      <View style={styles.reviewContainer}>
+        <ScrollView>
+          <ReviewCard
+            title="John Doe"
+            subtitle="2 days ago"
+            description="Food was great as always, highly recommend this place! Will definitely come back soon. Only note is to keep the music a little less loud."
+            fullscreen
+          />
+          <ScrollView
+            style={styles.replyOptions}
+            contentContainerStyle={styles.replyOptionsContainer}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+          >
+            <TouchableOpacity style={styles.replyOption}>
+              <Text style={styles.replyOptionText}>Thank you John!</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.replyOption}>
+              <Text style={styles.replyOptionText}>Thanks John!</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.replyOption}>
+              <Text style={styles.replyOptionText}>Many thanks John!</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.replyOption}>
+              <Text style={styles.replyOptionText}>Appreciate it John!</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.replyOption}>
+              <Text style={styles.replyOptionText}>Customize..</Text>
+            </TouchableOpacity>
+          </ScrollView>
         </ScrollView>
-      </ScrollView>
-
+        <View style={styles.buttons}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>View on Facebook</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Dismiss</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
       <TextInput placeholder="Reply.." style={styles.textInput} multiline />
-    </View>
+    </KeyboardAwareScrollView>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    // flex: 1,
-  },
-  reviewContainer: {
-    // flex: 1,
-  },
+  container: {},
+  reviewContainer: {},
   textInput: {
     fontSize: 14,
     backgroundColor: 'white',
@@ -96,5 +109,20 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: 'white',
     fontSize: 12,
+  },
+  buttons: {
+    flexDirection: 'row',
+  },
+  button: {
+    backgroundColor: 'white',
+    padding: 15,
+    flex: 1,
+    alignItems: 'center',
+    borderColor: '#f1f1f1',
+    margin: 2,
+    marginLeft: 0,
+  },
+  buttonText: {
+    color: 'grey',
   },
 })
