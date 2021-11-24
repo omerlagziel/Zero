f8b90869a28b16b3f2c77ea4cd457b757a8ac9e2
L <l@ls-macbook-pro.mynet>
2021-11-24 18:03:22 +0200

    #1 - Design web, looks good

--- packages/components/src/App.web.tsx
+++ packages/components/src/App.web.tsx
@@ -5,15 +5,18 @@ import { Provider } from 'react-redux'
 
 import { store } from './store'
 import Reviews from './features/reviews/Reviews'
+import { useMediaQuery } from 'react-responsive'
 
 type Props = {
   children: React.ReactNode
 }
 
 const App = ({ children }: Props) => {
+  const isMobile = useMediaQuery({ maxWidth: 900 })
+
   return (
     <Provider store={store}>
-      <View style={styles.main}>
+      <View style={[styles.main, isMobile ? mobileStyles.main : {}]}>
         <View style={styles.side} />
         <View style={styles.pageContainer}>{children}</View>
         <View style={styles.side} />
@@ -26,12 +29,19 @@ const styles = StyleSheet.create({
   main: {
     backgroundColor: '#f1f1f1',
     flexDirection: 'row',
+    height: '100vh',
   },
   pageContainer: {
     flex: 2,
   },
   side: {
     flex: 1,
+  },
+})
+
+const mobileStyles = StyleSheet.create({
+  main: {
+    flexDirection: 'column',
   },
 })
 
